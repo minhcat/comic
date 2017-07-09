e8c9bfd6333e80feccbb0706135a833005527a89
minhcat <minhcat@github.com>
2017-07-09 12:21:06 +0700

    tạo logo và menu cơ bản

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -19,7 +19,6 @@
 		margin: auto;
 	}
 	header{
-		background-color: red;
 	}
 	main{
 		background-color: white;
@@ -32,8 +31,49 @@
 <body>
 	<div class="wrapper">
 		<header>
-			<div class="logo">Logo</div>
-			<nav>Menu</nav>
+			<div class="logo">
+				<img src="image/1020x100.png">
+			</div>
+			<nav id="navbar" class="navbar navbar-default">
+					<div class="text-center">
+						<ul class="nav navbar-nav">
+							<li class="active home"><a class="page1" href=""><i class="glyphicon glyphicon-home"></i></a></li>
+							<li class=""><a class="page2" href="">Menu 1</a></li>
+							<li class=""><a class="page3" href="">Menu 2</a></li>
+							<li class=""><a class="page4" href="">Menu 3</a></li>
+							<li class=""><a class="page5" href="">Menu 4</a></li>
+							<li class="dropdown">
+								<a class="page6" href="#" class="dropdown-toggle" data-toggle="dropdown">Menu 5 <span class="caret"></span></a>
+								<ul class="dropdown-menu">
+									<li><a href="" class="">Submenu 1</a></li>
+									<li><a href="" class="">Submenu 2</a></li>
+									<li><a href="" class="">Submenu 3</a></li>
+								</ul>
+							</li>
+							<li class="dropdown">
+								<a class="page7" href="#" class="dropdown-toggle" data-toggle="dropdown">Menu 6 <span class="caret"></span></a>
+								<ul class="dropdown-menu">
+									<li><a href="" class="">Submenu 4</a></li>
+									<li><a href="" class="">Submenu 5</a></li>
+									<li><a href="" class="">Submenu 6</a></li>
+								</ul>
+							</li>
+						</ul>
+						<div class="right">
+							<form class="navbar-form" method="GET" action="">
+								<div class="input-group">
+									
+									<input type="text" name="search" class="form-control" placeholder="tìm kiếm">
+									<div class="input-group-btn">
+										<button class="btn btn-default" type="submit">
+											<i class="glyphicon glyphicon-search"></i>
+										</button>
+									</div>
+								</div>
+							</form>
+						</div>
+					</div>
+				</nav>
 		</header>
 		<main>
 			<div class="slide">Slide</div>
