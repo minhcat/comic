c56490436654c853633d9968fb61328a8b7a5e8a
minhcat <minhcat@github.com>
2017-07-09 11:53:00 +0700

    chỉnh css cho header, main, footer

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -3,10 +3,34 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Comic</title>
+	<style type="text/css">
+	body{
+		margin: 0px;
+		background-color: gray;
+	}
+	.wrapper{
+		width: 1020px;
+		margin: auto;
+	}
+	header{
+		height: 300px;
+		background-color: red;
+	}
+	main{
+		height: 600px;
+		background-color: white;
+	}
+	footer{
+		height: 300px;
+		background-color: blue;
+	}
+	</style>
 </head>
 <body>
-	<header>header</header>
-	<main>main</main>
-	<footer>footer</footer>
+	<div class="wrapper">
+		<header>header</header>
+		<main>main</main>
+		<footer>footer</footer>
+	</div>
 </body>
 </html>
