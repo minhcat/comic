27f5b3e5174321e2de83ad75723712cb509467b9
minhcat <minhcat@github.com>
2017-07-09 11:57:02 +0700

    thêm thư viện bootstrap

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -3,6 +3,12 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Comic</title>
+	<!-- Bootstrap -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<!-- CSS -->
 	<style type="text/css">
 	body{
 		margin: 0px;
